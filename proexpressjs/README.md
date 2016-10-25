8f0f8cf5d1c1bccc291ef036821f01f1fb302adb
Apress <gwenanspearing@apress.com>
2016-10-25 21:41:28 +0100

    First commit

---
+++ proexpressjs/README.md
@@ -0,0 +1,13 @@
+> Examples for the Pro Express.js book [Apress, 2014]
+
+# Pro Express.js
+
+The book is in progress, if you would like to receive alpha, email/tweet/Skype/text the author ([@azat_co](http://twitter.com/azat_co)) directly.
+
+![](http://www.apress.com/media/catalog/product/cache/9/image/9df78eab33525d08d6e5fb8d27136e95/A/9/A9781484200384-3d.png)
+
+<http://proexpressjs.com>
+
+<http://amzn.to/1pZdGFo>
+
+<http://www.apress.com/9781484200384>
---
+++ proexpressjs/ch2/hello-name.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var port = 3000;
+var app = express();
+
+app.get('/name/:user_name', function(request,response) {
+  response.status(200);
+  response.set('Content-Type', 'text/html');
+  response.end('<html><body>' +
+    '<h1>Hello ' + req.params.user_name + '</h1>' +
+    '</body></html>'
+  );
+});
+
+app.get('*', function(request, response){
+  response.end('Hello World');
+});
+
+app.listen(port, function(){
+  console.log('The server is running, ' +
+    ' please open your browser at http://localhost:%s',
+     port);
+});
